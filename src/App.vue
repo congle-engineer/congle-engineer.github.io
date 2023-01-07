8a878cbde1c124980c18b29e1f919d8960236485
Cong Le <congle@blockchainlabs.asia>
2023-01-07 12:55:14 +0700

    Update

--- src/App.vue
+++ src/App.vue
@@ -61,13 +61,43 @@ export default {
       {{name}}'s To Do List
     </h4>
     <div class="container-fluid p-4">
-      <div class="row">
-        <div class="col font-weight-bold">Task</div>
-        <div class="col-2 font-weight-bold">Done</div>
+      <div class="row" v-if="filteredTasks.length == 0">
+        <div class="col text-center">
+          <b>Nothing to do!</b>
+        </div>
       </div>
-      <div class="row" v-for="t in tasks" v-bind:key="t.action">
-        <div class="col">{{t.action}}</div>
-        <div class="col-2">{{t.done}}</div>
+      <template v-else>
+        <div class="row">
+          <div class="col font-weight-bold">Task</div>
+          <div class="col-2 font-weight-bold">Done</div>
+        </div>
+        <div class="row" v-for="t in filteredTasks" v-bind:key="t.action">
+          <div class="col">{{t.action}}</div>
+          <div class="col-2 text-center">
+            <input type="checkbox" v-model="t.done" class="form-check-input" />
+          </div>
+        </div>
+      </template>
+      <div class="row py-2">
+        <div class="col">
+          <input v-model="newItemText" class="form-control" />
+        </div>
+        <div class="col-2">
+          <button class="btn btn-primary" v-on:click="addNewTask">Add</button>
+        </div>
+      </div>
+      <div class="row bg-secondary py-2 mt-2 text-white">
+        <div class="col text-center">
+          <input type="checkbox" v-model="hideCompleted" class="form-check-input" />
+          <label class="form-check-label font-weight-bold">
+            Hide completed tasks
+          </label>
+        </div>
+        <div class="col text-center">
+          <button class="btn btn-sm btn-warning" v-on:click="deleteCompleted">
+            Delete Completed
+          </button>
+        </div>
       </div>
     </div>
   </div>
@@ -79,10 +109,43 @@ export default {
     data() {
       return {
         name: "Le Cong",
-        tasks: [{ action: "Buy Flowers", done: false },
-                { action: "Get Shoes", done: false },
-                { action: "Collect Tickets", done: true },
-                { action: "Call Joe", done: false }]
+        // tasks: [{ action: "Buy Flowers", done: false },
+        //         { action: "Get Shoes", done: false },
+        //         { action: "Collect Tickets", done: true },
+        //         { action: "Call Joe", done: false }],
+        tasks: [],
+        hideCompleted: true,
+        newItemText: ""
+      }
+    },
+    computed: {
+      filteredTasks() {
+        return this.hideCompleted?
+          this.tasks.filter(t => !t.done) : this.tasks
+      }
+    },
+    methods: {
+      addNewTask() {
+        this.tasks.push({
+          action: this.newItemText,
+          done: false
+        });
+        this.storeData();
+        // localStorage.setItem("todos", JSON.stringify(this.tasks));
+        this.newItemText = "";
+      },
+      storeData() {
+        localStorage.setItem("todos", JSON.stringify(this.tasks));
+      },
+      deleteCompleted() {
+        this.tasks = this.tasks.filter(t => !t.done);
+        this.storeData();
+      }
+    },
+    created() {
+      let data = localStorage.getItem("todos");
+      if (data != null) {
+        this.tasks = JSON.parse(data)
       }
     }
   }
