32dbf287d973895ef20a93504971e61ad30018d3
Cong Le <congle@blockchainlabs.asia>
2023-01-07 09:51:32 +0700

    Update

--- src/App.vue
+++ src/App.vue
@@ -26,7 +26,7 @@ export default {
 </style>
  -->
 
-<template>
+<!-- <template>
   <div id="app">
     <img src="./assets/logo.png">
     <HelloWorld msg="Le Cong ahihi 123" />
@@ -47,10 +47,43 @@ export default {
 <style>
   #app {
     font-family: 'Avenir', Helvetica, Arial, sans-serif;
-/*    -webkit-font-smoothing: antialiased;*/
+    -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
     color: #2c3e50;
     margin-top: 60px;
   }
-</style>
+</style> -->
+
+<template>
+  <div id="app">
+    <h4 class="bg-primary text-white text-center p-2">
+      {{name}}'s To Do List
+    </h4>
+    <div class="container-fluid p-4">
+      <div class="row">
+        <div class="col font-weight-bold">Task</div>
+        <div class="col-2 font-weight-bold">Done</div>
+      </div>
+      <div class="row" v-for="t in tasks" v-bind:key="t.action">
+        <div class="col">{{t.action}}</div>
+        <div class="col-2">{{t.done}}</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'app',
+    data() {
+      return {
+        name: "Le Cong",
+        tasks: [{ action: "Buy Flowers", done: false },
+                { action: "Get Shoes", done: false },
+                { action: "Collect Tickets", done: true },
+                { action: "Call Joe", done: false }]
+      }
+    }
+  }
+</script>
