009de780f69cd818411d9e7b7614016ac4d6c72d
congle-engineer <congle-engineer@gmail.com>
2024-04-26 21:19:13 +0700

    Update first page

--- pages/first.js
+++ pages/first.js
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export default function First() {
   return (
     <div>
-      This is the first page.
+      This is the first page, ahihi ..........
       <br/>
       <Link href="/">
         <button>Go to Homepage</button>
