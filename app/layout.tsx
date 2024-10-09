42d365e96b98396a20e566c041b1763c33cb68be
congle-engineer <congle-engineer@gmail.com>
2024-10-09 14:44:03 +0700

    Lint code

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { CMS_NAME, HOME_OG_IMAGE_URL } from "lib/constants";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import cn from "classnames";
-import { ThemeSwitcher } from "components/ThemeSwitcher";
+// import { ThemeSwitcher } from "components/ThemeSwitcher";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
