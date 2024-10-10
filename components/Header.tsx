01c4b7acee8de0ecc397a42b18f14c3e7e155eaf
congle-engineer <congle-engineer@gmail.com>
2024-10-10 13:45:26 +0700

    Update logo

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,13 +1,21 @@
 import Link from "next/link";
 import { ThemeSwitcher } from "./ThemeSwitcher";
 import { Container } from "./Container";
+import Image from "next/image";
 
 export function Header() {
   return (
     <Container>
       <div className="mb-16 mt-8 flex flex-row items-center justify-between md:mb-12">
         <div>
-          <Link href="/">Logo</Link>
+          <Link href="/">
+            <Image
+              src="/assets/logo/travel-blog-logo-Photoroom.png"
+              alt="Logo"
+              width={100}
+              height={100}
+            />
+          </Link>
         </div>
         <div className="flex items-center space-x-4">
           <Link href="/">Home</Link>
