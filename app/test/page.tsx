649ce29eff40fcc62c62ac3177f8efb7672b5e40
congle-engineer <congle-engineer@gmail.com>
2025-03-06 13:50:44 +0700

    Update

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -26,17 +26,25 @@ import {
   AccordionTrigger,
 } from "@/components/ui/accordion";
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
-import { Terminal } from "lucide-react";
+import { Terminal, AlertCircle } from "lucide-react";
 
 export default function Test() {
   return (
     <main>
       <Container>
-        <Alert>
+        <Alert className="mb-4">
           <Terminal className="h-4 w-4" />
           <AlertTitle>Heads up!</AlertTitle>
           <AlertDescription>
             You can add components and dependencies to your app using the cli.
+          </AlertDescription>
+        </Alert>
+
+        <Alert className="mb-4" variant="destructive">
+          <AlertCircle className="h-4 w-4" />
+          <AlertTitle>Error</AlertTitle>
+          <AlertDescription>
+            Your session has expired. Please log in again.
           </AlertDescription>
         </Alert>
 
