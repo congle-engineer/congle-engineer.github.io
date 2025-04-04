9fe89787459d49f32e2bc6895394b36b7489ecba
congle-engineer <congle-engineer@gmail.com>
2025-04-04 15:44:00 +0700

    Update

--- components/MoreStories.tsx
+++ components/MoreStories.tsx
@@ -2,7 +2,7 @@
 
 import { Post } from "interfaces/post";
 import { PostPreview } from "./PostPreview";
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import { Pagination } from "./Pagination";
 
 type Props = {
@@ -12,21 +12,47 @@ type Props = {
 export function MoreStories({ posts }: Props) {
   const itemsPerPage = 5;
   const totalItems = posts.length;
-  console.log("totalItems: ", totalItems);
   const totalPages = Math.ceil(totalItems / itemsPerPage);
 
-  const [currentPage, setCurrentPage] = useState<number>(1);
+  const nextSectionRef = useRef<HTMLDivElement>(null);
+
+  const [currentPage, setCurrentPage] = useState<number>(() => {
+    if (typeof window !== "undefined") {
+      return Number(localStorage.getItem("currentPage"));
+    } else {
+      return 1;
+    }
+  });
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const savedPage = Number(localStorage.getItem("currentPage")) || 1;
+      setCurrentPage(savedPage);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      localStorage.setItem("currentPage", String(currentPage));
+    }
+  }, [currentPage]);
 
   const handlePageChange = (page: number) => {
     if (page >= 1 && page <= totalPages) {
       setCurrentPage(page);
-      window.scrollTo({ top: 800, behavior: "smooth" });
+
+      setTimeout(() => {
+        nextSectionRef.current?.scrollIntoView({
+          behavior: "smooth",
+          block: "start",
+        });
+      }, 100);
     }
   };
 
   return (
     <div>
-      <div className="grid grid-cols-1 gap-y-4">
+      <div ref={nextSectionRef} className="grid grid-cols-1 gap-y-4">
         {posts
           .slice((currentPage - 1) * itemsPerPage, currentPage * itemsPerPage)
           .map((post) => (
